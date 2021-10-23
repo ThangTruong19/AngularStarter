9a2b539feac997c5507cfc96cee899dcd1f47157
ThangTruong19 <thangtruong19.winsoft@gmail.com>
2021-10-23 22:09:15 +0700

    Adding Editor for product and order

--- src/app/admin/productEditor.component.ts
+++ src/app/admin/productEditor.component.ts
@@ -1,8 +1,26 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 
 @Component({
-    template: `<div class="bg-warning p-2 text-white">
-                <h3>Product Editor Placeholder</h3>
-                </div>`
+    templateUrl: "productEditor.component.html" 
 })
-export class ProductEditorComponent{}
+export class ProductEditorComponent{
+    editing: boolean = false;
+    product: Product = new Product();
+
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, 
+                private repository: ProductRepository){
+        this.editing = activatedRoute.snapshot.params["mode"] == "edit";
+        if(this.editing){
+            this.product = this.repository.getProduct(activatedRoute.snapshot.params["id"]);
+        }
+    }
+
+    save(form: NgForm){
+        this.repository.saveProduct(this.product);
+        this.router.navigateByUrl("/admin/main/products");
+    }
+}
